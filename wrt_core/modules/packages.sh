1e8482b7a0795adf238717e3297ffd391501c6cd
happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>
2026-07-02 07:14:19 +0800

    Update packages.sh
    Signed-off-by: happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -3,7 +3,7 @@
 remove_unwanted_packages() {
     local luci_packages=(
         "luci-app-passwall" "luci-app-ddns-go" "luci-app-rclone" "luci-app-ssr-plus"
-        "luci-app-vssr" "luci-app-daed" "luci-app-dae" "luci-app-alist" "luci-app-homeproxy"
+        "luci-app-vssr" "luci-app-daed" "luci-app-dae" "luci-app-alist"
         "luci-app-haproxy-tcp" "luci-app-openclash" "luci-app-mihomo" "luci-app-appfilter"
         "luci-app-msd_lite" "luci-app-unblockneteasemusic" "luci-app-adguardhome"
     )
@@ -155,7 +155,7 @@ install_custom_feed() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd
+        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-app-unblockneteasemusic
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
