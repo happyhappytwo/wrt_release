8a0ebbb1cada59f19615a7be37c781fd29bb1c7c
happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>
2026-07-02 07:18:25 +0800

    Update packages.sh
    Signed-off-by: happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -159,7 +159,7 @@ install_custom_feed() {
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
-        luci-app-quickstart luci-app-store luci-app-homeproxy luci-app-mosdns
+        luci-app-quickstart luci-app-store luci-app-mosdns
         luci-app-passwall nikki luci-app-nikki mihomo-meta
         open-app-filter luci-app-oaf lucky luci-app-lucky luci-app-easytier
     )
