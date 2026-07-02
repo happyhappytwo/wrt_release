0d2fb68b914510b54c70396510df03346d4d9d79
happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>
2026-07-02 23:05:44 +0800

    Update packages.sh
    Signed-off-by: happyhappytwo <128892911+happyhappytwo@users.noreply.github.com>

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -153,9 +153,9 @@ install_custom_feed() {
         v2dat adguardhome luci-app-adguardhome ddns-go \
         luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd luci-app-store quickstart \
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
-        lucky luci-app-lucky luci-app-openclash luci-app-amlogic \
+        lucky luci-app-lucky luci-app-openclash luci-app-amlogic UnblockNeteaseMusic-Go UnblockNeteaseMusic luci-app-unblockmusic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-app-unblockmusic
+        msd_lite luci-app-msd_lite cups luci-app-cupsd
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
